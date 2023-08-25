46253bf4c384c2a4bda1862099d9001ed4635d9a
JuanMachuca1 <juandavidflores2005@gmail.com>
2023-08-25 13:46:55 -0600

    clases lab

---
+++ src/lab6p2_juanflores/Consola.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab6p2_juanflores;
+
+import java.util.ArrayList;
+
+
+public class Consola {
+    
+    private String identificacion;
+    private String fabricante;
+    private int añosDeUso;
+    private double precio;
+    private ArrayList juegos = new ArrayList();
+    private String modelo;
+
+    public Consola() {
+    }
+
+    public Consola(String identificacion, String fabricante, int añosDeUso, double precio, String modelo) {
+        this.identificacion = identificacion;
+        this.fabricante = fabricante;
+        this.añosDeUso = añosDeUso;
+        this.precio = precio;
+        this.modelo = modelo;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public int getAñosDeUso() {
+        return añosDeUso;
+    }
+
+    public void setAñosDeUso(int añosDeUso) {
+        this.añosDeUso = añosDeUso;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public ArrayList getJuegos() {
+        return juegos;
+    }
+
+    public void setJuegos(ArrayList juegos) {
+        this.juegos = juegos;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Consola{" + "identificacion=" + identificacion + ", fabricante=" + fabricante + ", a\u00f1osDeUso=" + añosDeUso + ", precio=" + precio + ", juegos=" + juegos + ", modelo=" + modelo + '}';
+    }
+    
+    
+    
+}
---
+++ src/lab6p2_juanflores/Portatil.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab6p2_juanflores;
+
+
+public class Portatil extends Consola{
+    
+    private String tamañoPantalla;
+    private int bateria;
+    private boolean estuche;
+    
+}
